753c175b4a9bcf1ffaae8911b03cf059ca300b5e
DaurF <213053@astanait.edu.kz>
2022-11-04 22:38:15 +0600

    Updated ProfileFragment, Added lifecycle callbacks to fragments

--- app/src/main/java/com/csi/bottomnavigationactivity/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/csi/bottomnavigationactivity/ui/profile/ProfileFragment.kt
@@ -6,13 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.csi.bottomnavigationactivity.R
 import com.csi.bottomnavigationactivity.databinding.FragmentProfileBinding
 import timber.log.Timber
 
 class ProfileFragment : Fragment() {
-    private lateinit var viewModel: ProfileViewModel
+    private lateinit var profileViewModel: ProfileViewModel
 
     private var _binding: FragmentProfileBinding? = null
     private val binding
@@ -33,9 +34,58 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
+        profileViewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.e("onViewCreated()")
+        profileViewModel.text.observe(viewLifecycleOwner, Observer {
+            binding.textProfile.text = it
+        })
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Timber.e("onViewStateRestored()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.e("onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.e("onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.e("onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.e("onStop()")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Timber.e("onDestroyView()")
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.e("onDestroy()")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Timber.e("onDetach()")
     }
 }
--- app/src/main/java/com/csi/bottomnavigationactivity/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/csi/bottomnavigationactivity/ui/profile/ProfileViewModel.kt
@@ -1,7 +1,13 @@
 package com.csi.bottomnavigationactivity.ui.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ProfileViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "Profile Fragment"
+    }
 
+    val text: LiveData<String> = _text
 }
