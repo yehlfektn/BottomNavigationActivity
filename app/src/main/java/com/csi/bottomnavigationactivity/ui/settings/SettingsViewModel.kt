cf67697768c9e3381048b7662395ab7be859649c
Ulpan Beken <u.beken@typi.team>
2022-11-04 23:16:31 +0600

    added more logs for missing lifecycle methods in HomeFragment added a new tab "Settings"

---
+++ app/src/main/java/com/csi/bottomnavigationactivity/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,14 @@
+package com.csi.bottomnavigationactivity.ui.settings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is settings Fragment"
+    }
+    val text: LiveData<String> = _text
+}
