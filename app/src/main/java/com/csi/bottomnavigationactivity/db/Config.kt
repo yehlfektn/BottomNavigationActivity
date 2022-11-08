44a622100a0caefa6ddfd68be62acbf39f3432cd
yerakulmanov <200103127@stu.sdu.edu,.kz>
2022-11-08 17:56:53 +0600

    add config class

---
+++ app/src/main/java/com/csi/bottomnavigationactivity/db/Config.kt
@@ -0,0 +1,11 @@
+package com.csi.bottomnavigationactivity.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "configTable")
+class Config (
+    val nameConfig: String,
+    val descriptionConfig: String,
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+)
